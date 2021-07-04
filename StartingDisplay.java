252eca55518e4d0412f34816856020cce74da14f
abuzar7972 <abushaikh7972@gmail.com>
2021-07-04 11:56:17 +0530

    Starting Display

---
+++ StartingDisplay.java
@@ -0,0 +1,9 @@
+package com.bl.employeeWage;
+
+public class StartingDisplay {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
+}
